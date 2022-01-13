20c3d98ab848fc0948a811c0ac439a74d8c9b1f0
Shradhdha Parsana <shradhdha03@gmail.com>
2022-01-13 11:40:05 -0500

    With ATTD Code

---
+++ calculator_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CalculatorApp from './CalculatorApp';
+
+function App() {
+    return (
+        <div className="App">
+            <CalculatorApp />
+        </div>
+    );
+}
+
+export default App;
---
+++ calculator_app/src/backend/YOUR_TEST_FILE.test.js
@@ -0,0 +1,11 @@
+let expect = require('expect');
+
+it('should pass', () => {
+    expect('hello').toBe('hello');
+});
+
+
+
+
+
+
